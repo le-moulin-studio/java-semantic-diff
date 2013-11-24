45a03690a294b54b7e88187c7335eeb1a5d77e87
Vincent Cantin <vincent.cantin@le-moulin-studio.com>
2013-11-24 18:51:38 +0800

    Added class TextContentElementData.

---
+++ src/main/java/com/lemoulinstudio/sdiff/content/TextContentElementData.java
@@ -0,0 +1,13 @@
+package com.lemoulinstudio.sdiff.content;
+
+/**
+ * The text-based data of a content element.
+ */
+public class TextContentElementData implements ContentElementData {
+  
+  /**
+   * The data of this content element.
+   */
+  public String data;
+  
+}
